b0c44577843de47ecc70057f19a79b22079f83fb
Bruno <b-coimbra@hotmail.com>
2020-02-25 14:15:23 -0300

    added circle animations to the background

--- src/Background.jsx
+++ src/Background.jsx
@@ -18,8 +18,14 @@ function Background() {
       radius,
       sAngle = 0,
       eAngle = Math.PI * 2,
-      clockwise = false
-    }) => ctx.arc(x, y, radius, sAngle, eAngle, clockwise);
+      clockwise = false,
+      fillStyle = 'black'
+    }) => {
+      ctx.beginPath();
+      ctx.arc(x, y, radius, sAngle, eAngle, clockwise);
+      ctx.fillStyle = fillStyle;
+      ctx.fill();
+    };
 
     const rgba = (r, g, b, a) => `rgba(${r}, ${g}, ${b}, ${a})`;
 
@@ -31,28 +37,81 @@ function Background() {
 
     let dim = 0.1,
         radius = 1,
-        padding = 3;
+        alpha = 1,
+        padding = 10;
+
+    let circles = [];
+    let enlargedPoints = [];
 
     for (let x = 0; x <= size.x; x++) {
       if (x >= (size.x / 2))
-        dim -= (1 / size.x);
-      else
-        dim += (1 / size.x);
+        alpha *= -1;
+
+      dim += (alpha / size.x);
 
       for (let y = 0; y <= size.y; y++) {
-        ctx.beginPath();
-
         let coordinates = {
           x: (x * spacing) + radius + padding,
           y: (y * spacing) + radius + padding 
         };
 
-        Circle({ ...coordinates, radius });
+        Circle({
+          ...coordinates,
+          radius,
+          fillStyle: rgba(0, 0, 0, dim.toFixed(2))
+        });
 
-        ctx.fillStyle = rgba(0, 0, 0, dim.toFixed(2));
-        ctx.fill();
+        circles.push({ ...coordinates });
       }
     }
+
+    const inRange = (num, range, pad = 0) =>
+          (range <= num && range >= num - pad);
+
+    const pinpoint = (x, y) =>
+          circles.filter(circle => (inRange(x, circle.x, padding * 2)) && (inRange(y, circle.y, padding * 2)))[0];
+
+    const enlarge = (location, radius = 1) => {
+      let velocity  = 20,
+          maxRadius = 30,
+          minRadius = 1,
+          scaleBy   = 1,
+          fillStyle = '#47485a';
+
+      let timer = setInterval(() => {
+        if (radius >= maxRadius / 2)
+          scaleBy *= -1;
+
+        radius += scaleBy;
+
+        if (radius >= maxRadius || radius <= minRadius)
+          clearInterval(timer);
+
+        ctx.clearRect(location.x - Math.round(maxRadius / 2),
+                      location.y - Math.round(maxRadius / 2),
+                      maxRadius, maxRadius);
+
+        Circle({ ...location, radius, fillStyle });
+      }, velocity);
+
+      enlargedPoints.pop();
+    };
+
+    const hasEnlarged = (point) => {
+      if (enlargedPoints.length === 0)
+        return false;
+
+      return enlargedPoints.find(e => point.x === e.x && point.y === e.y);
+    };
+
+    canvas.current.onmousemove = ({ x, y }) => {
+      const location = pinpoint(x, y);
+
+      if (typeof location != 'undefined' && !hasEnlarged(location)) {
+        enlarge(location);
+        enlargedPoints.push(location);
+      }
+    };
   };
 
   const setCanvasSize = () => {
